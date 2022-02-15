e60bcbfc2ead322a20df712335bdea1f277d27d6
jouad01 <jouardiouardi@cifpfbmoll.eu>
2022-02-15 01:29:39 +0100

    build: Algunos cambios en los paquetes

---
+++ src/test/java/edu/poniperro/testRicksy/TestCrystalExpender.java
@@ -0,0 +1,31 @@
+package edu.poniperro.testRicksy;
+
+import edu.poniperro.ricksybusiness.CreditCard;
+import edu.poniperro.ricksybusiness.CrystalExpender;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+public class TestCrystalExpender {
+
+    private CrystalExpender expender = null;
+
+
+    @Before
+    public void setUp(){
+        expender = new CrystalExpender(100, 500);
+        assertNotNull("Expender creado", expender);
+    }
+
+    @Test
+    public void dispatch(){
+        CreditCard card = new CreditCard("Jouad", "4916119711304546");
+        expender.dispatch(card);
+        assertEquals(99, expender.stock());
+    }
+
+
+
+}
