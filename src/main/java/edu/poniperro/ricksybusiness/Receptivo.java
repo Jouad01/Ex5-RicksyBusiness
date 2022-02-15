291424365557fcea5c190a9164aa85779f36ba8c
jouad01 <jouardiouardi@cifpfbmoll.eu>
2022-02-15 01:26:21 +0100

    feat: Terminando logica clase Ufos y Receptivo

--- src/main/java/edu/poniperro/ricksybusiness/Receptivo.java
+++ src/main/java/edu/poniperro/ricksybusiness/Receptivo.java
@@ -1,9 +1,18 @@
 package edu.poniperro.ricksybusiness;
 
+import java.util.ArrayList;
+
 public class Receptivo {
-    private Receptivo(){} // Pendiente
+    public Receptivo(){} // Pendiente
+    private final ArrayList<GuestDispatcher> dispatchers = new ArrayList<GuestDispatcher>();
 
-   /* public void registra(GuestDispatcher){
+    public void registra(GuestDispatcher dispatcher){
+        dispatchers.add(dispatcher);
+    }
 
-    }*/
+    public void dispatch(CreditCard card){
+        for (GuestDispatcher dispatcher: dispatchers){
+            dispatcher.dispatch(card);
+        }
+    }
 }
--- src/main/java/edu/poniperro/ricksybusiness/UfosPark.java
+++ src/main/java/edu/poniperro/ricksybusiness/UfosPark.java
@@ -3,29 +3,41 @@ package edu.poniperro.ricksybusiness;
 import java.util.HashMap;
 import java.util.Map;
 
-public class UfosPark {
-    private double fee = 500d;
+public class UfosPark implements GuestDispatcher {
+    private final double fee = 500d;
     private final Map<String, String> flota = new HashMap<String, String>(); // Poner string a hashmap (interfaz)
 
 
     public UfosPark() {
     }
 
-    public void add(String ovni){
+    public void add(String ovni) {
         this.flota.put(ovni, null); // Añade ovni a flota sin asignar nada (null)
     }
 
-    public void String fee(){
-        this.fee = fee;
+    public String getUfoOf(String number) {
+        String ufoID = null;
+        if (this.flota.containsValue(number)) {
+            for (Map.Entry<String, String> entry : this.flota.entrySet()) {
+                if (entry.getValue() == number) {
+                    ufoID = entry.getKey();
+                    break;
+                }
+            }
+        }
+        return ufoID;
     }
 
-    /*public String getUfoOf(String number) {
+
+  /*  public boolean containsCard(String number) {
     }*/
 
-   public void dispatch(CreditCard card) {
+    @Override
+    public void dispatch(CreditCard card) {
 
     }
 
     public boolean containsCard(String number) {
+        return false;
     }
 }
